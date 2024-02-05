40deb6236fbb6463cfefb4d6363489131760a1b0
Muhammad Bilal <mohammadbillal268@gmail.com>
2024-02-05 21:11:16 +0500

    done

---
+++ app.js
@@ -0,0 +1,60 @@
+var hours = 0;
+var mins = 0;
+var seconds = 0;
+var miliSeconds = 0;
+var interval;
+var hoursDisp = document.getElementById("hoursDisp");
+console.log(hoursDisp);
+var minsDisp = document.getElementById("minsDisp");
+console.log(minsDisp);
+var secDisp = document.getElementById("secondsDisp");
+console.log(secDisp);
+var miliSecDisp = document.getElementById("miliSecondsDisp");
+console.log(miliSecDisp);
+function display() {
+  hoursDisp.innerHTML = hours;
+  minsDisp.innerHTML = mins;
+  secDisp.innerHTML = seconds;
+  miliSecDisp.innerHTML = miliSeconds;
+}
+display();
+
+function handleStart() {
+  console.log("Start");
+  interval = setInterval(() => {
+    miliSeconds++;
+    miliSecDisp.innerHTML = miliSeconds;
+    if (miliSeconds === 10) {
+      miliSeconds = 0;
+      seconds++;
+      secDisp.innerHTML = seconds;
+      if (seconds == 60) {
+        seconds = 0;
+        mins++;
+        minsDisp.innerHTML = mins;
+        if (mins == 60) {
+          mins = 0;
+          hours++;
+          hoursDisp.innerHTML = hours;
+        }
+      }
+    }
+  }, 100);
+}
+
+function handleStop() {
+  clearInterval(interval);
+}
+
+function handleReset() {
+  console.log("Reset");
+  handleStop();
+  hours = 0;
+  mins = 0;
+  seconds = 0;
+  miliSeconds = 0;
+  hoursDisp.innerHTML = 0;
+  minsDisp.innerHTML = 0;
+  secondsDisp.innerHTML = 0;
+  miliSecDisp.innerHTML = 0;
+}
